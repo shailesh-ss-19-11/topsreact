575d14744781a24bb98253124fe7200b0f6c8950
Shailesh <68338746+shailesh-ss-19-11@users.noreply.github.com>
2023-10-06 09:29:14 +0530

    Add files via upload

---
+++ jspractice/js/index.js
@@ -0,0 +1,40 @@
+// console.log("hello");
+
+// in const type of variable we cant chnage the value 
+// const name = "shailesh";
+
+// comment ---> comment is unexecutable code for the compiler
+
+// in let we can change the value and let has its own local scope 
+// let name = "shailesh";
+// name = "xyz"
+// console.log(name);
+
+// in var we can define same name varibale multiple times
+// var has the global scope
+// var name = "shailesh";
+// name ="xyz"
+// console.log(name)
+
+// data type////////////////////////////////////// 
+// number 
+// String
+// Boolean
+// undefined
+// null
+
+
+// const name = "shailesh"
+// const age = 23
+// const contact = 111111111111
+// const address = "nagpur"
+// const isMarried = true 
+// const hisowncompany = null
+
+// console.log(name)
+// console.log(age)
+// console.log(contact)
+// console.log(address)
+// console.log(isMarried)
+// console.log(hisowncompany)
+// console.log(typeof isMarried)

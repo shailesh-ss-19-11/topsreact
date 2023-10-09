f4d997028a7f5dab49abf710f3ba7f23a457e554
Shailesh <68338746+shailesh-ss-19-11@users.noreply.github.com>
2023-10-09 09:29:12 +0530

    Update index.js

--- jspractice/js/index.js
+++ jspractice/js/index.js
@@ -1,4 +1,4 @@
-console.log("hello");
+// console.log("hello");
 
 // in const type of variable we cant chnage the value
 // const name = "shailesh";
@@ -76,7 +76,7 @@ console.log("hello");
 // console.log(num1>num2 && num1===num2);
 // // false     true 
 // console.log(num1===num2 && num2===num1);
-            // true         true 
+// true         true 
 
 // || operator 
 // const num1 = 10;
@@ -100,3 +100,98 @@ console.log("hello");
 // console.log("num1 is less"):
 // console.log("its true") 
 // console.log("its false");
+
+// conditional statements /////////////////////////
+// if-else-if /////////////////////////////////////
+// const num1 = 20;
+// const num2 = 10;
+// if (num1 > num2) {         //true or false
+//     console.log(`${num1} is greater ${num2}`)
+// }
+// else if (num1 === num2) {
+//     console.log(`${num1} is equal to ${num2}`)
+// }
+// else {
+//     console.log(`${num2} is greater ${num1}`)
+// }
+
+// switch case /////////////////////////////////////
+// const day = "wedhhhhnesday" ;
+// switch (day) {
+//     case "monday":
+//         console.log("monday")
+//         break;
+//     case "tuesday":
+//         console.log("tuesday")
+//         break;
+//     case "wednesday":
+//         console.log("wednesday")
+//         break;
+//     case "thursday":
+//         console.log("thursday")
+//         break;
+//     case "friday":
+//         console.log("friday")
+//         break;
+//     case "saturday":
+//         console.log("saturday")
+//         break;
+//     case "sunday":
+//         console.log("sunday")
+//         break;
+//     default:
+//         console.error("pease check the day once")
+//         break;
+// }
+
+// loop //////////////////////////////////////////////
+// for loop ////////////////////////////
+
+// for (let i = 0; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// while loop ////////////////////////////
+
+// let i = 0;
+// while (i < 100) {
+//     console.log(i);
+//     i++;
+// }
+
+// 1+2+3+4+5.. .10 ///55
+
+
+// var i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<100);
+
+// string /////////////////////////////////////////////
+// const name = "shailesh gokhale"; 
+// console.log(name);
+// console.log(name.length);
+// console.log(name[0]);
+// console.log(name.slice(0,5));
+// console.log(name.charAt(5));
+// const sapreatedname = name.split(" ");
+// const firstname = sapreatedname[0]
+// const lastname = sapreatedname[1];
+// console.log(firstname);
+// console.log(lastname);
+
+
+// let fullname = firstname+" "+lastname;
+// let fullname = firstname.concat(" ").concat(lastname)
+// console.log(fullname);
+
+// const name1 = "poonam meshram"
+
+// if(name1.includes("poonam")){
+//     console.log("she is in tops technology");
+// }
+// forloop in string////////////////////////////////////////////////
+// for(let i=0;i<fullname.length;i++){
+//     console.log(fullname[i] + " ");
+// }

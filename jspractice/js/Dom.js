b69f6392acb54a46cb9cdc1e1f1085655984b48d
Shailesh <68338746+shailesh-ss-19-11@users.noreply.github.com>
2023-10-25 09:28:54 +0530

    Update Dom.js

--- jspractice/js/Dom.js
+++ jspractice/js/Dom.js
@@ -1,7 +1,7 @@
 // # --- >id
 // . --- >class
 // tag <h1>--->class
-console.dir(document)
+// console.dir(document)
 
 
 // let obj= {
@@ -11,18 +11,55 @@ console.dir(document)
 // obj.name="abc"
 // console.log(obj)
 
-const heading1 = document.getElementById("heading1");
-console.dir(heading1);
-heading1.textContent="this heading is changed"
+// const heading1 = document.getElementById("heading1");
+// console.dir(heading1);
+// heading1.textContent="this heading is changed"
 
-const headings = document.getElementsByClassName("heading") // it return htmlcolection that is array like object
-console.log(headings)
-for (let i = 0; i < headings.length; i++) {
-    if (headings[i].textContent === "this is third  heading") {
-        headings[i].style.backgroundColor = "red"
-        headings[i].style.color = "white"
-        headings[i].style.textDecoration = "underline"
-        // headings[i].style.display = "none"
-    }
-}
-// school1978@
+// const headings = document.getElementsByClassName("heading") // it return htmlcolection that is array like object
+// console.log(headings)
+// console.log(Array.isArray(headings))
+// for (let i = 0; i < headings.length; i++) {
+//     if (headings[i].textContent === "this is third  heading") {
+//         headings[i].style.backgroundColor = "red"
+//         headings[i].style.color = "white"
+//         headings[i].style.textDecoration = "underline"
+//         // headings[i].style.display = "none"
+//     }
+// }
+// // school1978@
+// queryselector using id////////////////////////////////////////////////
+// const element = document.querySelector("#heading1")
+// console.log(element)
+// element.textContent="this is first demo of query selector"
+
+
+// queryselector using class////////////////////////////////////////////////
+// const element = document.querySelector(".heading")
+// console.dir(element)
+// element.textContent="this is first demo of query selector"
+// queryselectorAll using class////////////////////////////////////////////////
+// const element = document.querySelectorAll(".heading")
+// for (let i = 0; i < element.length; i++) {
+//     console.log(element[i])
+//     if(element[i].textContent=="this is second heading"){
+//         element[i].style.backgroundColor = "red"
+//         element[i].style.color = "white"
+//     }
+// }
+
+
+// DOM traversing ////////////////////////////////////////////
+// document object model///////////////////////////////////// 
+
+const rootElement  = document.getRootNode();
+const htmlelement = rootElement.childNodes[0];
+const bodyElement = htmlelement.childNodes[2];
+console.log(bodyElement.childNodes)
+// let 
+// for (let i = 0; i < bodyElement.childNodes.length; i++) {
+//         console.log(bodyElement.childNodes[i])
+//         if(bodyElement.childNodes[i].textContent=="\n    "){
+//             delete bodyElement.childNodes[i]
+//         }
+// }
+console.log(bodyElement.childNodes)
